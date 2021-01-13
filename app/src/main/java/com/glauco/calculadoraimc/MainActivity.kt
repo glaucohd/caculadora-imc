8d06aa1239c65be1e1bd1ea173c96acf2261185d
Glauco <glauco@iMac-de-Glauco.local>
2021-01-12 23:55:16 -0300

    configurando codigo da aplicação

--- app/src/main/java/com/glauco/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/glauco/calculadoraimc/MainActivity.kt
@@ -2,10 +2,60 @@ package com.glauco.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        lateinit var peso: EditText
+        lateinit var altura: EditText
+        lateinit var resultado: TextView
+
+
+        var calcular = findViewById<Button>(R.id.bt_calcular)
+        var deletar = findViewById<ImageView>(R.id.icone_limpar)
+
+        calcular.setOnClickListener {
+            peso = findViewById(R.id.edit_peso)
+            val pesoInt = Integer.parseInt(peso.text.toString())
+
+            altura = findViewById(R.id.edit_altura)
+            var alturaFloat = java.lang.Float.parseFloat((altura.text.toString()))
+
+
+            resultado = findViewById(R.id.mensagem)
+            val imc = pesoInt / (alturaFloat * alturaFloat)
+
+            val mensagem = when {
+                imc <= 18.5 -> "Peso baixo"
+                imc <= 24.9 -> "Peso normal"
+                imc <= 29.9 -> "Sobrepeso"
+                imc <= 34.9 -> "Obesidade (Grau 1)"
+                imc <= 39.9 -> "Obesidade (Grau 2)"
+                else -> "Obesidade Mórbida (Grau 3)"
+
+            }
+
+            resultado.text = "IMC:" + imc.toString() + "\n" + mensagem
+        }
+
+        deletar.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                peso.setText("")
+                altura.setText("")
+                mensagem.setText("")
+            }
+            false
+        }
+
     }
 }
